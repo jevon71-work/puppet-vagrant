fcaf7e0c81dad0d012507a78f2be7aaeb8e73de4
John Maclean <jayeola@gmail.com>
2016-09-19 13:52:52 +0100

    provisioning for a centos puppet agent

--- bootfiles/centosAgent.sh
+++ bootfiles/centosAgent.sh
@@ -1,14 +1,23 @@
 #!/bin/bash
-#
-# this node should already have the epel repo
-yum remove -y -q epel-release
-yum clean all
-yum update -y
-yum install -y -q epel-release screen bash-completion
+# source: https://goo.gl/MCZtUH
 
-yum install -y puppet
-cp -v /vagrant/etc/puppet/puppet_agent.conf /etc/puppet/puppet.conf
+
+yum remove -y -q epel-release
+yum clean all && yum update -y
+
+rpm -ivh http://yum.puppetlabs.com/puppetlabs-release-el-7.noarch.rpm
+
+yum clean all && yum update -y
+
+yum install -y puppet screen git
+puppet resource package puppet ensure=latest
+
+
+systemctl enable puppet
+systemctl start puppet
+
+# cp -v /vagrant/etc/puppet/puppet_agent.conf /etc/puppet/puppet.conf
 
 cat<<EOX>/root/kickpuppet.sh
-puppet agent --test --no-daemonize --waitforcert 60
+puppet agent --test --no-daemonize --waitforcert 60 --server puppet.vm.vagrant
 EOX
