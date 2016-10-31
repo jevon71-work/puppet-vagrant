d40a5e988667463b29b1f93ab94600b9efcdbabe
jevon71-work <jevon.white@dhigroupinc.com>
2016-10-31 16:40:45 +0000

    Added lsof and curl packages to default

--- puppet/environments/testenv/manifests/default.pp
+++ puppet/environments/testenv/manifests/default.pp
@@ -1,4 +1,4 @@
-$base_packagelist = ['screen', 'git', 'bash-completion', 'unzip', 'wget', 'tree', 'telnet', 'traceroute']
+$base_packagelist = ['screen', 'git', 'bash-completion', 'unzip', 'wget', 'tree', 'telnet', 'traceroute', 'lsof', 'curl']
 $os_spec_packagelist = $facts['osfamily'] ? {
   'redhat' => ['python34', 'ansible', 'bind-utils'],
   'debian' => ['dnsutils'],
