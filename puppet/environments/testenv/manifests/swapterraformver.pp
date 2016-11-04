b2935ccfa0457b5d8f020bcb59e5241ec60e765b
jevon71-work <jevon.white@dhigroupinc.com>
2016-11-04 11:52:12 +0000

    Added a file to make it easy to swap Terraform.

---
+++ puppet/environments/testenv/manifests/swapterraformver.pp
@@ -0,0 +1,43 @@
+accounts::user {'puppet':
+  uid => '501',
+  gid => '501',
+}
+
+file { '/opt/terraform':
+  ensure => 'directory',
+  owner  => 'root',
+  group  => 'root',
+  mode   => '0755',
+}
+
+file { '/opt/terraform/ver_0.7.7':
+  ensure => 'directory',
+  owner  => 'root',
+  group  => 'root',
+  mode   => '0755',
+}
+
+file { '/opt/terraform/terraform-new':
+  ensure => 'link',
+  target => '/opt/terraform/ver_0.7.7/terraform',
+  # require => Staging::Extract['terraform_0.7.7_linux_amd64.zip'],
+}
+
+file { '/opt/terraform/terraform':
+  ensure => 'link',
+  target => '/opt/terraform/ver_0.7.7/terraform',
+  # require => Staging::Extract['terraform_0.7.7_linux_amd64.zip'],
+}
+
+file { '/opt/terraform/ver_0.6.16':
+  ensure => 'directory',
+  owner  => 'root',
+  group  => 'root',
+  mode   => '0755',
+}
+
+file { '/opt/terraform/terraform-old':
+  ensure => 'link',
+  target => '/opt/terraform/ver_0.6.16/terraform',
+  # require => Staging::Extract['terraform_0.6.16_linux_amd64.zip'],
+}
