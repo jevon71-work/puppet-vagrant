b29201e254b50f091e98ec02d3316a67a6d1531b
jevon71-work <jevon.white@dhigroupinc.com>
2016-11-17 15:48:24 +0000

    Change alias name from terragrunt to efcterragrunt

--- puppet/enableEFCTerragruntAlias.pp
+++ puppet/enableEFCTerragruntAlias.pp
@@ -1,6 +1,6 @@
-$setup_workstation_username = hiera('setup_workstation_username', 'dummy')
+$setup_workstation_username      = hiera('setup_workstation_username',      'dummy')
 $setup_workstation_user_fullname = hiera('setup_workstation_user_fullname', 'You have not specified your common.yaml')
-$setup_workstation_user_sshkeys = hiera('setup_workstation_user_sshkeys', 'ssh-rsa This will never work')
+$setup_workstation_user_sshkeys  = hiera('setup_workstation_user_sshkeys',  'ssh-rsa This will never work')
 
 accounts::user {$setup_workstation_username:
   uid     => '5001',
@@ -16,6 +16,6 @@ file { "/home/${setup_workstation_username}/.bash_profile":
 
 file_line { "add_Terragrunt_alias_to_${setup_workstation_username}":
   path    => "/home/${setup_workstation_username}/.bash_profile",
-  line    => 'alias terragrunt=\'AWS_PROFILE=efcprodaws terragrunt\'',
+  line    => 'alias efcterragrunt=\'AWS_PROFILE=efcprodaws terragrunt\'',
   require => File["/home/${setup_workstation_username}/.bash_profile"],
 }
